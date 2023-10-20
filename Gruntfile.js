9be662295ae8e1f7a164e873106219d13b214988
VyacheslavVolkhin <vv@htmlworker.com>
2023-10-20 09:10:25 +0500

    adD

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function (grunt) {
-    /*const mozjpeg = require('imagemin-mozjpeg');*/
     grunt.initConfig({
-
+        pkg: grunt.file.readJSON('package.json'),
         fixturesPath: "_includes",
 
 
@@ -17,6 +16,8 @@ module.exports = function (grunt) {
                         scripts: '<%= fixturesPath %>/**/scripts.html',
                         head: '<%= fixturesPath %>/**/head.html',
                         header: '<%= fixturesPath %>/**/header.html',
+                        filter: '<%= fixturesPath %>/**/filter.html',
+                        question: '<%= fixturesPath %>/**/question.html',
                         footer: '<%= fixturesPath %>/**/footer.html',
                     },
                     data: {
@@ -81,8 +82,8 @@ module.exports = function (grunt) {
                     optimization: 2
                 },
                 files: {
-                    "css/style.css": "css/less/style.less",
-                    "css/style.min.css": "css/less/style.less"
+                    "css/style.css": "./css/less/style.less",
+                    "css/style.min.css": "./css/less/style.less"
                 }
             }
         },
@@ -129,6 +130,27 @@ module.exports = function (grunt) {
             }
         },
 
+        /*uglify: {
+            tgJS: {
+                files: {
+                    'js/min/app.min.js': ['js/jquery.min.js', 'js/!*.js', '!js/scripts.js'],
+                    'js/min/scripts.min.js': ['js/scripts.js'],
+                },
+            },
+        },*/
+
+        /*cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'css/min/app.min.css': ['css/reset.css', 'css/!*.css', '!css/style.min.css', '!css/style.css'],
+                }
+            }
+        },*/
+
 
 
         watch: {
@@ -147,11 +169,21 @@ module.exports = function (grunt) {
                     nospawn: true
                 }
             },
+            /*js: {
+                files: ['js/!*.js'],
+                tasks: ['uglify'],
+            },
+            css: {
+                files: ['css/!*.css'],
+                tasks: ['cssmin'],
+            },*/
         }
     });
 
+    /*grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');*/
     require('load-grunt-tasks')(grunt);
     require('postcss-plugin')({});
 
-    grunt.registerTask('default', ['watch'], ['less'], ['sprite'],  ['htmlbuild'], ['postcss'], ['imagemin']);
+    grunt.registerTask('default', ['watch'], ['less'], ['sprite'],  ['htmlbuild'], ['postcss'], ['imagemin'] );
 };
